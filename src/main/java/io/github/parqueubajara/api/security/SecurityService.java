fb12a1b91a487f26fda0769fac1a3aeabd6017af
felipe-damasceno19 <felipegpt19@gmail.com>
2026-05-13 14:42:44 -0300

    fix: adjust audit user configuration

--- src/main/java/io/github/parqueubajara/api/security/SecurityService.java
+++ src/main/java/io/github/parqueubajara/api/security/SecurityService.java
@@ -15,14 +15,34 @@ public class SecurityService {
 
     private final UserService userService;
 
-    public SystemUser getUserLogged(){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    public SystemUser getUserLogged() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
-        if(authentication != null && !authentication.isAuthenticated()) {
-            if (authentication instanceof CustomAuthentication customAuth) {
-                return customAuth.getUser();
-            }
+        if (auth == null || !auth.isAuthenticated() || "anonymousUser".equals(auth.getPrincipal())) {
+            return null;
         }
+
+        Object principal = auth.getPrincipal();
+
+        if (auth instanceof CustomAuthentication customAuth) {
+            return customAuth.getUser();
+        }
+
+        if (principal instanceof SystemUser user) {
+            return user;
+        }
+
+        String email = null;
+        if (principal instanceof UserDetails details) {
+            email = details.getUsername();
+        } else if (principal instanceof String strEmail) {
+            email = strEmail;
+        }
+
+        if (email != null) {
+            return userService.findByEmail(email);
+        }
+
         return null;
     }
 }
