6bf1a7f4ec394ccf4b3fc892abb533eeac3bf061
Gabriel Siqueira <gabriel.siqueira3000@gmail.com>
2026-05-18 16:40:15 -0300

    fix: correct typo /attractives to /attractions in SecurityConfig

--- src/main/java/io/github/parqueubajara/api/config/SecurityConfig.java
+++ src/main/java/io/github/parqueubajara/api/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
                         ).permitAll()
                         .requestMatchers(HttpMethod.GET,
                                 "/tourist-spots/**",
-                                "/attractives/**",
+                                "/attractions/**",
                                 "/host-points/**",
                                 "/restaurants/**",
                                 "/events/**",
